1c7649e3a3b81d0df565dc9f6ee0d63b4d600143
shingu <t_shingu@bold.ne.jp>
2026-05-21 14:16:27 +0900

    rewrite sql2

--- src/main/resources/sql/team2_data.sql
+++ src/main/resources/sql/team2_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO team2_users_tbl VALUES('U001', 'テスト1');
-INSERT INTO team2_users_tbl VALUES('U002', 'テスト2');
+INSERT INTO team2_users_tbl VALUES('001', 'テスト1');
+INSERT INTO team2_users_tbl VALUES('002', 'テスト2');
