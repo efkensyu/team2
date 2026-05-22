9d6bc1eca4b57fe93d8133d08858d4222f75d457
sekine <r_sekine@bold.ne.jp>
2026-05-22 13:51:43 +0900

    add home buttons

--- src/main/java/com/example/demo/team2/controller/Team2HomeController.java
+++ src/main/java/com/example/demo/team2/controller/Team2HomeController.java
@@ -15,20 +15,66 @@ import com.example.demo.team2.repository.Team2UserRepository;
 
 @Controller
 public class Team2HomeController {
-	
+
 	@Autowired
 	private Team2UserRepository userRepository;
-	
+
 	@GetMapping("/team2/home")
 	public String index(HttpSession session, Model model) {
-		
+
 		//ログイン情報を取得
 		Team2LoginForm team2LoginForm = (Team2LoginForm) session.getAttribute("team2LoginForm");
 		List<Team2User> users = userRepository.findAll();
 		model.addAttribute("users", users);
-		
+
 		model.addAttribute("team2LoginForm", team2LoginForm);
 		System.out.println("ホーム画面へ遷移：" + team2LoginForm.getLoginId());
 		return "team2/team2_home";
 	}
+
+	//プロフィール
+	@GetMapping("/team2/profile")
+	public String profile(HttpSession session, Model model) {
+		Team2LoginForm team2LoginForm = (Team2LoginForm) session.getAttribute("team2LoginForm");
+	    model.addAttribute("team2LoginForm", team2LoginForm);
+		System.out.println("プロフィール画面へ遷移");
+		return "team2/users/team2_profile";
+	}
+
+	//タイムライン
+	@GetMapping("/team2/logs")
+	public String logs(HttpSession session, Model model) {
+		Team2LoginForm team2LoginForm = (Team2LoginForm) session.getAttribute("team2LoginForm");
+	    model.addAttribute("team2LoginForm", team2LoginForm);
+		System.out.println("タイムライン画面へ遷移");
+		return "team2/study_logs/team2_logs";
+	}
+
+	//学習記録登録
+	@GetMapping("/team2/logs/create")
+	public String logsCreate(HttpSession session, Model model) {
+		Team2LoginForm team2LoginForm = (Team2LoginForm) session.getAttribute("team2LoginForm");
+	    model.addAttribute("team2LoginForm", team2LoginForm);
+		System.out.println("学習記録登録画面へ遷移");
+		return "team2/study_logs/team2_logs_create";
+	}
+
+	//問題一覧
+	@GetMapping("/team2/questions")
+	public String questions(HttpSession session, Model model) {
+		Team2LoginForm team2LoginForm = (Team2LoginForm) session.getAttribute("team2LoginForm");
+	    model.addAttribute("team2LoginForm", team2LoginForm);
+		System.out.println("問題一覧画面へ遷移");
+		return "team2/questions/team2_questions";
+	}
+
+	//採点結果一覧
+	@GetMapping("/team2/answers")
+	public String answers(HttpSession session, Model model) {
+		Team2LoginForm team2LoginForm = (Team2LoginForm) session.getAttribute("team2LoginForm");
+	    model.addAttribute("team2LoginForm", team2LoginForm);
+		System.out.println("採点結果一覧画面へ遷移");
+		return "team2/answers/team2_answers";
+	}
+
 }
