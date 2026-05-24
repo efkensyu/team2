e83bab31d3354f4c10474f68cf6938a8a884b80b
shingu <t_shingu@bold.ne.jp>
2026-05-24 21:05:26 +0900

    edit questions function

--- src/main/java/com/example/demo/team2/controller/Team2QuestionController.java
+++ src/main/java/com/example/demo/team2/controller/Team2QuestionController.java
@@ -8,25 +8,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class Team2QuestionController {
 	
 	//問題一覧を表示
-	@GetMapping("/team2/questions")
+	@GetMapping("/team2/questions/list")
 	public String index() {
 		return "team2/questions/team2_questions";
+		
 	}
 	
 	//問題作成を表示
-	@PostMapping("/team2/questions")
+	@PostMapping("/team2/questions/create")
 	public String createQuestion() {
-		return "team2/questions/team2_questions_create";
+		return "team2/questions/team2_questions_detail";
 	}
 	
 	//問題詳細を表示
-	@PostMapping("/team2/questions")
+	@PostMapping("/team2/questions/detail")
 	public String detailQuestion() {
 		return "team2/questions/team2_questions_detail";
 	}
 	
 	//問題を編集
-		@PostMapping("/team2/questions/detail")
+		@PostMapping("/team2/questions/edit")
 		public String editQuestion() {
 			return "team2/questions/team2_questions_edit";
 		}
