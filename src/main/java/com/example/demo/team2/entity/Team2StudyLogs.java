06784c4e3b4fc05455ac09eceff0a5dbb7f19046
sekine <r_sekine@bold.ne.jp>
2026-05-25 17:22:37 +0900

    edit entity class

--- src/main/java/com/example/demo/team2/entity/Team2StudyLogs.java
+++ src/main/java/com/example/demo/team2/entity/Team2StudyLogs.java
@@ -3,6 +3,7 @@ package com.example.demo.team2.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,29 +15,43 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name="TEAM2_STUDY_LOGS_TBL")
+@Table(name="team2_study_logs_tbl")
 @Data
 public class Team2StudyLogs {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "study_log_id")
 	private int studyLogId;
-	
+
+	@Column(name = "user_id")
 	private int userId;
+
+	@Column(name = "field_id")
 	private int fieldId;
+
+	@Column(name = "study_date")
 	private LocalDate studyDate;
+
+	@Column(name = "study_time")
 	private int studyTime;
+
+	@Column(name = "study_name")
 	private String studyName;
+
+	@Column(name = "study_content")
 	private String studyContent;
-	private LocalDateTime  createdAt;
-	private LocalDateTime  updatedAt;
-	
-	//Fieldsと結合
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
 	@ManyToOne
-	@JoinColumn(name = "fieldId", insertable = false, updatable = false)
+	@JoinColumn(name = "field_id", insertable = false, updatable = false)
 	private Team2Fields field;
-	
-	//Usersと結合
+
 	@ManyToOne
-	@JoinColumn(name = "userId", insertable = false, updatable = false)
+	@JoinColumn(name = "user_id", insertable = false, updatable = false)
 	private Team2User user;
 }
--- src/main/java/com/example/demo/team2/entity/Team2User.java
+++ src/main/java/com/example/demo/team2/entity/Team2User.java
@@ -2,6 +2,7 @@ package com.example.demo.team2.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,17 +12,29 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name="TEAM2_USERS_TBL")
+@Table(name="team2_users_tbl")
 @Data
 public class Team2User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
 	private int userId;
-	
+
+	@Column(name = "login_id")
 	private String loginId;
+
+	@Column(name = "password")
 	private String password;
+
+	@Column(name = "user_name")
 	private String userName;
+
+	@Column(name = "is_deleted")
 	private int isDeleted;
+
+	@Column(name = "created_at")
 	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 }
